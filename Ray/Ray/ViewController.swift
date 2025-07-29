a874432697b892c1268217c32d49ad60e526fbc5
canada2374 <syi2374@icloud.com>
2025-07-29 18:08:29 -0400

    Add Ray

---
+++ Ray/Ray/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ray
+//
+//  Created by Yeongik Song on 2025-07-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
