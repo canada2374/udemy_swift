d8127d74a3c647a3d914406afe72efda880e9987
canada2374 <syi2374@icloud.com>
2025-08-01 17:53:47 -0400

    Update IAmPoor project

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Yeongik Song on 2025-08-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I Am Poor/I Am PoorTests/I_Am_PoorTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_PoorTests.swift
+//  I Am PoorTests
+//
+//  Created by Yeongik Song on 2025-08-01.
+//
+
+import Testing
+@testable import I_Am_Poor
+
+struct I_Am_PoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
